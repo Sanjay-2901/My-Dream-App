31c0b61da1e6ccecc69946c7766f307f4984e625
Sanjay-2901 <sanjay@yavar.in>
2022-10-22 20:27:05 +0530

    Recipe App First-Course-Project

--- My-App/src/app/recipes/recipe-list/recipe-list.component.ts
+++ My-App/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
+  recipesList: Recipe[] = [
+    new Recipe('Test recipe', 'Just a Test', '/assets/images/ai-lab.webp'),
+    new Recipe('Test recipe', 'Just a Test', '/assets/images/ai-lab.webp'),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
